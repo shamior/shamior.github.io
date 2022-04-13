bc08194b2c1ee52e5fa6c952752027f7a8663aec
kev <shamior_@hotmail.com>
2022-04-12 21:02:25 -0300

    changed profile picture and bio, fixed some css flexbox things

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let copyButton = document.querySelector(".copy-icon")
-
+let emailDiv = document.querySelector("#email")
 
 copyButton.addEventListener(
     "click",
     () => navigator.clipboard.writeText("kastkuya@gmail.com"),
     false
 )
+
+emailDiv.onmouseover = () => copyButton.style.display = "flex"
+emailDiv.onmouseleave = () => copyButton.style.display = "none"

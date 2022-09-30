d73a746f3ed38c6a8a07f03c9bab2d573da03869
kev <shamior_@hotmail.com>
2022-09-30 18:15:50 -0300

    added space handler

--- scriptzap.js
+++ scriptzap.js
@@ -5,7 +5,8 @@ let gerarLinkButton = document.querySelector("#gerar-zap")
 
 gerarLinkButton.onclick = () => {
     let inputValue = document.querySelector("#inputzap").value
-    window.open('whatsapp://send/?phone=55' +inputValue+'&text=Ola,tudo bem? Sou o entregador do Ponto do açaí.%0AVocê fez um pedido? Se sim, poderia me mandar localização?&type=phone_number&app_absent=0')
+    inputValue = inputValue.replaceAll(" ", '')
+    window.open('whatsapp://send/?phone=55' +inputValue+'&text=Ola, tudo bem?%0ASou o entregador do Ponto do açaí.%0AVocê fez um pedido? Se sim, poderia me mandar localização?&type=phone_number&app_absent=0')
 }
 
 

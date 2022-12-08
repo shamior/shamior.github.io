aa23ac6e9c2cf389a3e8aa4fdb928f9b99a9b328
kev <shamior_@hotmail.com>
2022-12-08 10:17:03 -0300

    alterando nagazap

--- scriptzap.js
+++ scriptzap.js
@@ -1,12 +1,28 @@
 
 
 /*zapao*/
-let gerarLinkButton = document.querySelector("#gerar-zap")
+let gerarLinkButton = document.querySelector("#gerar-zap-msg")
+let gerarLinkButtonNoMsg = document.querySelector("#gerar-zap-nomsg")
 
-gerarLinkButton.onclick = () => {
-    let inputValue = document.querySelector("#inputzap").value
-    inputValue = inputValue.replaceAll(" ", '')
-    window.open('whatsapp://send/?phone=55' +inputValue+'&text=Ola, tudo bem?%0ASou o entregador do Ponto do açaí.%0AVocê fez um pedido? Se sim, poderia me mandar localização?&type=phone_number&app_absent=0')
+gerarLinkButton.onclick = async () => {
+    let inputValue = ''
+
+    navigator.clipboard.readText().then((data) => {
+        inputValue = data.replaceAll(" ", '')
+        window.open('whatsapp://send/?phone=55' +inputValue+'&text=Ola, tudo bem?%0ASou o entregador do Ponto do açaí.%0AVocê fez um pedido? Se sim, poderia me mandar localização?&type=phone_number&app_absent=0')
+    })
+
+}
+
+
+gerarLinkButtonNoMsg.onclick = async () => {
+    let inputValue = ''
+
+    navigator.clipboard.readText().then((data) => {
+        inputValue = data.replaceAll(" ", '')
+        window.open('whatsapp://send/?phone=55' +inputValue + '&type=phone_number&app_absent=0')
+    })
+
 }
 
 

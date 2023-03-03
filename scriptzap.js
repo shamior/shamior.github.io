34e430f589a27a2663923fae335e39fed5b0b790
shamior <shamior_@hotmail.com>
2023-03-03 13:01:48 -0300

    added input again

--- scriptzap.js
+++ scriptzap.js
@@ -5,27 +5,17 @@ let gerarLinkButton = document.querySelector("#gerar-zap-msg")
 let gerarLinkButtonNoMsg = document.querySelector("#gerar-zap-nomsg")
 
 gerarLinkButton.onclick = () => {
-    let inputValue = ''
-
-    navigator.clipboard.readText().then((data) => {
-        inputValue = data.replaceAll(" ", '')
-        window.open('whatsapp://send/?phone=55' +inputValue+'&text=Ola, tudo bem?%0ASou o entregador do Ponto do açaí.%0AVocê fez um pedido? Se sim, poderia me mandar localização?&type=phone_number&app_absent=0')
-    })
+    let inputValue = document.querySelector("#telefone").value;
+    inputValue = inputValue.replaceAll(" ", '')
+    window.open('whatsapp://send/?phone=55' +inputValue+'&text=Ola, tudo bem?%0ASou o entregador do Ponto do açaí.%0AVocê fez um pedido? Se sim, poderia me mandar localização?&type=phone_number&app_absent=0')
 
 }
 
 
 gerarLinkButtonNoMsg.onclick = () => {
-    let inputValue = ''
-    try {
-        navigator.clipboard.readText().then((data) => {
-            inputValue = data.replaceAll(" ", '')
-            window.open('whatsapp://send/?phone=55' +inputValue + '&type=phone_number&app_absent=0')
-        })
-    }catch(e){
-        window.alert(e);
-    }
-
+    let inputValue = document.querySelector("#telefone").value;
+    inputValue = inputValue.replaceAll(" ", '')
+    window.open('whatsapp://send/?phone=55' +inputValue + '&type=phone_number&app_absent=0')
 }
 
 

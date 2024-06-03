e218a477fab989acb21694622442ca6db81ffabd
shamior <shamior_@hotmail.com>
2024-06-03 18:21:23 -0300

    pagina principal modificada para depois adicionar um portfolio legal

--- pokemon/script.js
+++ pokemon/script.js
@@ -1,4 +1,4 @@
-function mountPokecardHTML(pokemon){
+function mountPokecardHTML(pokemon) {
     let innerHTML = `<div class="pokemon main-${pokemon.types[0].type.name}">`
     innerHTML += `<img src="${pokemon.sprites.other['official-artwork'].front_default}">`
     innerHTML += `<div class="id">#${pokemon.id}</div>`
@@ -16,47 +16,50 @@ function mountPokecardHTML(pokemon){
     return innerHTML
 }
 
-async function fetchPokemon(name_or_id){
+async function fetchPokemon(name_or_id) {
     return fetch("https://pokeapi.co/api/v2/pokemon/" + name_or_id)
-    .then(response => response.json())
-    .then(mountPokecardHTML)
+        .then(response => response.json())
+        .then(mountPokecardHTML)
 }
 
-function fetchInterval(start, end){
+function fetchInterval(start, end) {
     let promises = []
-    for (let i = start; i <= end; i++){
+    for (let i = start; i <= end; i++) {
         promises.push(fetchPokemon(i))
     }
     return promises
 }
 
-function fetchSingle(name_or_id){
+function fetchSingle(name_or_id) {
     return [fetchPokemon(name_or_id)]
 }
 
 
 
-document.querySelector("#botao")
-    .addEventListener('click', ()=>{
-        const pokemonsElement = document.querySelector("#pokemons")
-        const inputValue = document.querySelector("#maininput").value
-        let [start, end] = inputValue.split('-')
-        let promises
-        if (isNaN(start)){
+function onSubmit(event) {
+    event.preventDefault()
+    const pokemonsElement = document.querySelector("#pokemons")
+    const inputValue = document.querySelector("#maininput").value
+    let [start, end] = inputValue.split('-')
+    let promises
+    if (isNaN(start)) {
+        promises = fetchSingle(start)
+    } else {
+        if (end == undefined) {
             promises = fetchSingle(start)
-        }else{
-            if (end == undefined){
-                promises = fetchSingle(start)
-            }else{
-                promises = fetchInterval(Number(start), Number(end))
-            }
+        } else {
+            promises = fetchInterval(Number(start), Number(end))
         }
-        Promise.all(promises).then(htmls => {
-            let innerHTML = ""
-            htmls.forEach(e => {
-                innerHTML += e
-            })
-            pokemonsElement.innerHTML = innerHTML
+    }
+    Promise.all(promises).then(htmls => {
+        let innerHTML = ""
+        htmls.forEach(e => {
+            innerHTML += e
         })
-        
+        pokemonsElement.innerHTML = innerHTML
     })
+}
+
+
+document.querySelector("#botao").addEventListener('click', onSubmit)
+document.querySelector("form").addEventListener('submit', onSubmit)

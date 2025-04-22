acb7f79f7c0fd000608e33997fd56b9189681142
Kevyn Menezes <shamior_@hotmail.com>
2025-04-21 22:23:02 -0300

    fixed: changed value to total

--- gale/index.js
+++ gale/index.js
@@ -3,26 +3,29 @@ const divStart = '<div class="ciclo">\n'
 document.querySelector('button').addEventListener(
     "click", (event) => {
         let entrada = Number(document.querySelector("#entrada").value)
-        let porcentagem = Number(document.querySelector("#porcentagem").value.replace(',', '.'))
+        let porcentagem = (1/100) *  Number(document.querySelector("#porcentagem").value.replace(',', '.'))
         let container = document.querySelector("#container")
         let valor = entrada * porcentagem
         let cont = 1
         let ciclo = 0
         let insideDiv = divStart + `<h4>Ciclo: ${ciclo}</h4>`
         let html = ""
+        let total = 0
         console.log(valor, entrada)
-        while (valor <= entrada) {
+        while (total <= entrada) {
+            total += valor
             if (cont % 8 === 1 && cont !== 1) {
                 ciclo++
                 html += insideDiv
                 html += "</div>\n"
                 insideDiv = divStart + `<h4>Ciclo: ${ciclo}</h4>`
             }
-            insideDiv += `<p>${cont}: ${valor.toFixed(4)}</p>\n`
+            insideDiv += `<p>${cont}: ${total.toFixed(4)}</p>\n`
             valor *= 1.077
             cont++
         }
-        insideDiv += `<p>${cont}: ${valor.toFixed(4)}</p>\n`
+        total += valor
+        insideDiv += `<p>${cont}: ${total.toFixed(4)}</p>\n`
         html += insideDiv
         html += "</div>\n"
 

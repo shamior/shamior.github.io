e5e49e2656cd1f14b69fbb09c61dc7ba8b779ad4
Kevyn Menezes <shamior_@hotmail.com>
2025-04-21 22:08:22 -0300

    feat: added simple gale page

---
+++ gale/index.js
@@ -0,0 +1,31 @@
+const divStart = '<div class="ciclo">\n'
+
+document.querySelector('button').addEventListener(
+    "click", (event) => {
+        let entrada = Number(document.querySelector("#entrada").value)
+        let porcentagem = Number(document.querySelector("#porcentagem").value.replace(',', '.'))
+        let container = document.querySelector("#container")
+        let valor = entrada * porcentagem
+        let cont = 1
+        let ciclo = 0
+        let insideDiv = divStart + `<h4>Ciclo: ${ciclo}</h4>`
+        let html = ""
+        console.log(valor, entrada)
+        while (valor <= entrada) {
+            if (cont % 8 === 1 && cont !== 1) {
+                ciclo++
+                html += insideDiv
+                html += "</div>\n"
+                insideDiv = divStart + `<h4>Ciclo: ${ciclo}</h4>`
+            }
+            insideDiv += `<p>${cont}: ${valor.toFixed(4)}</p>\n`
+            valor *= 1.077
+            cont++
+        }
+        insideDiv += `<p>${cont}: ${valor.toFixed(4)}</p>\n`
+        html += insideDiv
+        html += "</div>\n"
+
+        container.innerHTML = html
+    }
+)

bea7f5acc420a33dce14a680825fe1af8e88dda3
Kevyn Menezes <shamior_@hotmail.com>
2025-04-22 09:28:26 -0300

    refactor(gale): changed styles

--- gale/index.js
+++ gale/index.js
@@ -1,5 +1,10 @@
 const divStart = '<div class="ciclo">\n'
 
+function getGale(cont){
+    if (cont%8 === 0) return "sg"
+    return `g${cont%8}`
+}
+
 document.querySelector('button').addEventListener(
     "click", (event) => {
         let entrada = Number(document.querySelector("#entrada").value)
@@ -8,27 +13,27 @@ document.querySelector('button').addEventListener(
         let valor = entrada * porcentagem
         let cont = 0
         let ciclo = 0
-        let insideDiv = divStart + `<h4>Ciclo: ${ciclo}</h4>`
+        let insideDiv = divStart + `<h4>Ciclo: ${ciclo}</h4><div class="entries">`
         let html = ""
-        let total = 0
-        console.log(valor, entrada)
+        let total = valor
         while (total <= entrada) {
-            total += valor
             if (cont % 8 === 0 && cont !== 0) {
                 ciclo++
+
                 html += insideDiv
-                html += "</div>\n"
-                insideDiv = divStart + `<h4>Ciclo: ${ciclo}</h4>`
+                html += "</div>\n</div>"
+                insideDiv = divStart + `<h4>Ciclo: ${ciclo}</h4><div class="entries">`
             }
-            insideDiv += `<p>${cont%8+1}: ${total.toFixed(4)}</p>\n`
+            insideDiv += `<p><span class="ciclo-cont">${getGale(cont)}:</span> ${total.toFixed(2)}</p>\n`
             valor *= 1.077
             cont++
+            total += valor
         }
-        total += valor
-        insideDiv += `<p>${cont%8+1}: ${total.toFixed(4)}</p>\n`
+
+        insideDiv += `<p><span class="ciclo-cont">${getGale(cont)}:</span> ${total.toFixed(2)}</p>\n`
         html += insideDiv
-        html += "</div>\n"
-        html += `<h4>Total gales: ${cont+1}</h4>`
+        html += "</div>\n</div>"
+        document.querySelector("#total-gale").innerHTML = `<h3>Total gales: ${cont+1}</h3>`
 
         container.innerHTML = html
     }
